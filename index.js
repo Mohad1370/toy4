784bb8f07ccb82f0e04ed58d6b787d2205b1fe20
mohad1370 <mohadesesadati70@gmail.com>
2022-07-01 12:00:20 -0400

    commit all!

---
+++ index.js
@@ -0,0 +1,10 @@
+<script>
+ var  age=prompt( 'enter your rate');
+ var rate =10
+if(rate== 0)
+{  console.log( 'non');}
+
+else {console.log('accses' );}
+document.write( 'how would you rate story 4');
+
+   </script>
